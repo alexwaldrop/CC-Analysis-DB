8f96a9529cefc8a9ddbf1ea868f321c500648114
Razvan Panea <razvan.panea@duke.edu>
2018-01-22 01:05:21 -0500

    Adds creator id and timestamp to SampleSheetCreator

--- AnalysisDB/SampleSheetCreator.py
+++ AnalysisDB/SampleSheetCreator.py
@@ -1,6 +1,7 @@
 from .. import Base
-from sqlalchemy import Column
-from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, TEXT, TINYINT
+from sqlalchemy import Column, ForeignKey, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, TEXT, TINYINT, TIMESTAMP
 
 
 class SampleSheetCreator(Base):
@@ -10,10 +11,15 @@ class SampleSheetCreator(Base):
     sample_sheet_creator_id = Column(INTEGER,       primary_key=True, unique=True)
     name                    = Column(VARCHAR(45),   nullable=False)
     description             = Column(TEXT,          nullable=True)
+    creation_timestamp      = Column(TIMESTAMP,     nullable=False, server_default=func.current_timestamp())
     class_name              = Column(TEXT,          nullable=False)
     min_input_size          = Column(INTEGER,       nullable=False)
     max_input_size          = Column(INTEGER,       nullable=True, default=None)
     submission_only         = Column(TINYINT,       nullable=False)
+
+    creator_id              = Column(ForeignKey("auth_user.id"), nullable=False, index=True)
+
+    creator                 = relationship("AuthUser")
 
     def __repr__(self):
         return self.__str__()
