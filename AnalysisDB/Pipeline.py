9c740f6293b4805daf3e44dcf9e41a3e7f90cbcd
Razvan Panea <razvan.panea@duke.edu>
2018-08-09 15:48:10 -0400

    Implement the default analysis type table 'Pipeline'

---
+++ AnalysisDB/Pipeline.py
@@ -0,0 +1,25 @@
+from .. import Base
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, LONGTEXT
+
+
+class Pipeline(Base):
+
+    __tablename__       = 'pipeline'
+
+    id                  = Column(INTEGER,       autoincrement=True, primary_key=True, nullable=False)
+    name                = Column(VARCHAR(128),  nullable=False)
+    description         = Column(LONGTEXT,      default=None)
+
+    analysis_type_id    = Column(INTEGER, ForeignKey("analysis_type.analysis_type_id"), nullable=False)
+    creator_id          = Column(INTEGER, ForeignKey("user.id"), nullable=False, index=True)
+
+    analysis_type       = relationship("AnalysisType",  backref="pipeline")
+    creator             = relationship("User")
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "<Pipeline(%(id)s)>" % self.__dict__
