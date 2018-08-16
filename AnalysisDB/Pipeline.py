a5c54a63af7588959e444d4365e9ca409f4a0183
Razvan Panea <razvan.panea@duke.edu>
2018-08-16 15:56:55 -0400

    Adds new column in the Pipeline table

--- AnalysisDB/Pipeline.py
+++ AnalysisDB/Pipeline.py
@@ -1,7 +1,7 @@
 from .. import Base
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, LONGTEXT
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, LONGTEXT, TINYINT
 
 
 class Pipeline(Base):
@@ -11,6 +11,7 @@ class Pipeline(Base):
     id                  = Column(INTEGER,       autoincrement=True, primary_key=True, nullable=False)
     name                = Column(VARCHAR(128),  nullable=False)
     description         = Column(LONGTEXT,      default=None)
+    is_upstream         = Column(TINYINT,       default=None)
 
     analysis_type_id    = Column(INTEGER, ForeignKey("analysis_type.analysis_type_id"), nullable=False)
     creator_id          = Column(INTEGER, ForeignKey("user.id"), nullable=False, index=True)
