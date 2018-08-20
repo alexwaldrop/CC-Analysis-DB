50dea98cb5d14e60fd513e617d00694677687837
Alex Waldrop <amw117@duke.edu>
2018-08-20 14:20:07 -0400

    Added git_commit to Analysis table.

--- AnalysisDB/Analysis.py
+++ AnalysisDB/Analysis.py
@@ -18,6 +18,8 @@ class Analysis(Base):
     final_output_dir    = Column(LONGTEXT,      nullable=False)
     sample_sheet        = Column(LONGTEXT,      nullable=False)
     run_start           = Column(TIMESTAMP,     default=None)
+    git_commit          = Column(VARCHAR(10),   default=None)
+
 
     rerun_parent_id     = Column(INTEGER, ForeignKey("analysis.analysis_id"), nullable=True, index=True)
     creator_id          = Column(INTEGER, ForeignKey("user.id"), nullable=False, index=True)
